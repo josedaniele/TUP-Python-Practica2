37e57f19ad915ceadaaa116425f881cfa6511036
SanchezMaximo <maximosanchez420@gmail.com>
2022-08-24 12:37:59 -0300

    Primer ejercicio realizado

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -1,7 +1,7 @@
 """Bloque IF, operadores lógicos, función max y operador ternario."""
 
 
-def maximo_basico(a: float, b: float) -> float:
+def maximo_basico(a: float, b: float) -> float:    # type: ignore
     """Toma dos números y devuelve el mayor.
 
     Restricciones:
@@ -9,7 +9,10 @@ def maximo_basico(a: float, b: float) -> float:
         - No utilizar ELSE
         - No utilizar la función max
     """
-
+    if a>b:
+     return(a)
+    elif a<b:
+     return(b)
 
 # NO MODIFICAR - INICIO
 assert maximo_basico(10, 5) == 10
@@ -17,15 +20,14 @@ assert maximo_basico(9, 18) == 18
 # NO MODIFICAR - FIN
 
 
-###############################################################################
+############################################################################### 
 
 
-def maximo_libreria(a: float, b: float) -> float:
+def maximo_libreria(a: float, b: float) -> float:  # type: ignore
     """Re-escribir utilizando el built-in max.
     Referencia: https://docs.python.org/3/library/functions.html#max
     """
-
-
+    return(max(a,b))
 # NO MODIFICAR - INICIO
 assert maximo_libreria(10, 5) == 10
 assert maximo_libreria(9, 18) == 18
@@ -35,13 +37,13 @@ assert maximo_libreria(9, 18) == 18
 ###############################################################################
 
 
-def maximo_ternario(a: float, b: float) -> float:
+def maximo_ternario(a: float, b: float) -> float:  # type: ignore
     """Re-escribir utilizando el operador ternario.
     Referencia: https://docs.python.org/3/reference/expressions.html#conditional-expressions # noqa: E501
     """
-
-
+    
+    return  a if a > b else b 
 # NO MODIFICAR - INICIO
 assert maximo_ternario(10, 5) == 10
 assert maximo_ternario(9, 18) == 18
-# NO MODIFICAR - FIN
+# NO MODIFICAR - FIN 
