613ac31132d4c54cadebf66ac4847e908761b06b
josedaniele <josedaniele493@gmail.com>
2022-08-31 12:42:10 -0300

    ejercicio 9

--- exercises/exercise9.py
+++ exercises/exercise9.py
@@ -1,5 +1,6 @@
 """Sum, Compresión de Listas, Map, Filter, Reduce."""
 
+from operator import imod, mod
 from typing import Iterable
 
 
@@ -15,7 +16,16 @@ def suma_cubo_pares_for(numeros: Iterable[int]) -> int:
         - https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions # noqa: E501
         - https://docs.python.org/3/library/functions.html#sum
     """
-
+    lista2 = []
+    total = 0
+    
+    for i in numeros:
+        elevado = i**3
+        if elevado % 2 == 0:
+            lista2.append(elevado)
+    for j in lista2:
+        total = total + j
+    return total
 
 # NO MODIFICAR - INICIO
 assert suma_cubo_pares_for([1, 2, 3, 4, 5, 6]) == 288
@@ -33,8 +43,8 @@ def suma_cubo_pares_sum_list(numeros: Iterable[int]) -> int:
         - https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions # noqa: E501
         - https://docs.python.org/3/library/functions.html#sum
     """
-
-
+    total= sum([x**3 for x in numeros if (x**3)%2==0]) 
+    return total
 # NO MODIFICAR - INICIO
 assert suma_cubo_pares_sum_list([1, 2, 3, 4, 5, 6]) == 288
 # NO MODIFICAR - FIN
@@ -52,7 +62,7 @@ Escribir una función lambda que eleve los elementos al cubo
 Restricción: Utilizar List, map y lambda y la variable numeros
 """
 
-numeros_al_cubo =  # Completar
+numeros_al_cubo = list(map(lambda x: x**3, numeros))  # Completar
 
 """
 Escribir una función lambda que permita filtrar todos los elementos pares
@@ -60,7 +70,7 @@ Escribir una función lambda que permita filtrar todos los elementos pares
 Restricción: Utilizar List, filter, lambda y la variable numeros_al_cubo
 """
 
-numeros_al_cubo_pares =  # Completar
+numeros_al_cubo_pares = list(filter(lambda x: (x%2)==0, numeros_al_cubo))  # Completar
 
 
 """
@@ -71,8 +81,7 @@ Restricción: Utilizar reduce, lambda y la variable numeros_al_cubo_pares
 
 from functools import reduce  # noqa: E402
 
-suma_numeros_al_cubo_pares =  # Completar
-
+suma_numeros_al_cubo_pares = reduce(lambda x,y: x+y ,numeros_al_cubo_pares)  # Completar
 
 # NO MODIFICAR - INICIO
 assert numeros_al_cubo == [1, 8, 27, 64, 125, 216]
